140f14d9b43b3cc20187d72a63ec68a604773b53
fade-cercleX <102993874+fade-cercleX@users.noreply.github.com>
2023-03-07 11:30:39 +0530

    fix bugs react toast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Scanner from "./Scanner";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
